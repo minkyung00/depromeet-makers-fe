bf9e351724139222f1f7a7440413a617248aa95d
minkyung00 <newminkyung@gmail.com>
2024-07-07 00:37:24 +0900

    fix(ui): add export component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,12 +1,14 @@
+import IconButton from './Button/IconButton';
 import { Accordion } from './Accordion';
 import { Badge } from './Badge';
 import { BottomNav } from './BottomNav';
 import BottomSheet from './BottomSheet';
 import Button from './Button';
 import Chip from './Chip';
+import ChipLine from './Chip';
 import { Header } from './Header';
 import Icon from './Icon';
 import Input from './Input';
 import Layout from './Layout';
 
-export { Accordion, Badge, BottomNav, BottomSheet, Button, Chip, Header, Icon, Input, Layout };
+export { Accordion, Badge, BottomNav, BottomSheet, Button, Chip, ChipLine, Header, Icon, IconButton, Input, Layout };
